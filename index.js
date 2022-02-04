7b36095435343aa5e8e8c63902c49d59a785fae5
kttkkkng <teerawat7971@gmail.com>
2022-02-04 14:02:22 +0700

    default

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// health check
+app.get("/", (req, res) => {
+  res.json({ message: "health check ok" });
+});
+
+// set port, listen for requests
+const PORT = 3000;
+db.sequelize.sync().then((req) => {
+    app.listen(PORT, () => console.log(`Backend started on port ${PORT}`))
+});
