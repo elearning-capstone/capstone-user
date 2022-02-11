1fc1430f902fce867165aeb4b4c864af0934645f
kttkkkng <teerawat7971@gmail.com>
2022-02-11 14:47:01 +0700

    try-catch

--- routes/user.js
+++ routes/user.js
@@ -3,32 +3,34 @@ const router = express.Router();
 const { user } = require("../models");
 
 router.post("/", async (req, res) => {
-    const { username, password } = req.body;
+    try {
+        const { username, password } = req.body;
 
-    if (typeof username != "string" || typeof password != "string") {
-        return res.status(400).json({ message: "invalid username, password" })
-    }
-
-    let count = await user.count({
-        where: {
-            username
+        if (typeof username != "string" || typeof password != "string") {
+            return res.status(400).json({ message: "invalid username, password" })
         }
-    });
 
-    if (count != 0) {
-        return res.status(400).json({ message: "invalid username, password" })
-    }
+        let count = await user.count({
+            where: {
+                username
+            }
+        });
 
-    let new_user = await user.create({
-        username,
-        password
-    });
+        if (count != 0) {
+            return res.status(400).json({ message: "invalid username, password" })
+        }
 
-    return res.json({
-        data: {
+        let new_user = await user.create({
+            username,
+            password
+        });
+
+        return res.json({
             user: new_user
-        }
-    });
+        });
+    } catch(err) {
+        return res.status(404).json({ message: "not found" });
+    }
 });
 
 module.exports = router;
