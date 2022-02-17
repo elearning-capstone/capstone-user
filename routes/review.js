1abd1501848f19ad7cd443fd04a042d6d929f7a3
noof3310 <noof3310@gmail.com>
2022-02-17 16:33:05 +0700

    add route review

---
+++ routes/review.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const axios = require("axios");
+const { userCheckMiddleware } = require("../middleware")
+const router = express.Router();
+
+const course_ip = "http://ip-172-31-36-250.ap-southeast-1.compute.internal:3000";
+
+router.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(course_ip + "/review");
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/create", userCheckMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(course_ip + "/review/create", req.body, { headers: { user: req.headers['user'] } });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+module.exports = router;
