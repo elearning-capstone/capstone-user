94eb95e7220febe96df2fa0157ab0baec3dfd5fa
kttkkkng <teerawat7971@gmail.com>
2022-02-17 21:58:37 +0700

    init comment

---
+++ routes/comment.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const axios = require("axios");
+const { userCheckMiddleware } = require("../middleware")
+const router = express.Router();
+
+const course_ip = "http://ip-172-31-36-250.ap-southeast-1.compute.internal:3000";
+
+router.get("/", async (req, res) => {
+    try {
+        console.log(req.params);
+        console.log(req.query);
+        //const response = await axios.get(course_ip + "/comment", { headers: { user: req.headers['user'] }, params: req.query });
+        return res.json({ message: "test" });
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+module.exports = router;
