4abd120d318d7be403664437c2c60e8eff0db6cd
noof3310 <noof3310@gmail.com>
2022-02-18 15:08:04 +0700

    add param get course review

--- routes/review.js
+++ routes/review.js
@@ -7,7 +7,7 @@ const course_ip = "http://ip-172-31-36-250.ap-southeast-1.compute.internal:3000"
 
 router.get("/", async (req, res) => {
     try {
-        const response = await axios.get(course_ip + "/review");
+        const response = await axios.get(course_ip + "/review", { params: req.query });
         return res.json(response.data);
     } catch (err) {
         return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
