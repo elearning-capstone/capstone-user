d51e9e5d729eee117b4f7b204b6cce85cb79cb5f
kttkkkng <teerawat7971@gmail.com>
2022-03-13 20:09:46 +0700

    comment get username

--- routes/comment.js
+++ routes/comment.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require("axios");
-const { userCheckMiddleware } = require("../middleware")
+const { userCheckMiddleware } = require("../middleware");
+const { user } = require("../models");
 const router = express.Router();
 
 const course_ip = "http://ip-172-31-36-250.ap-southeast-1.compute.internal:3000";
@@ -8,6 +9,51 @@ const course_ip = "http://ip-172-31-36-250.ap-southeast-1.compute.internal:3000"
 router.get("/", userCheckMiddleware, async (req, res) => {
     try {
         const response = await axios.get(course_ip + "/comment", { params: req.query });
+
+        let user_id = new Set();
+
+        for (const comment of response.data.comment) {
+            user_id.add(comment.user_id);
+            if (comment.tag_user) {
+                user_id.add(comment.tag_user);
+            }
+            for (const reply of comment.reply) {
+                user_id.add(reply.user_id);
+                if (reply.tag_user) {
+                    user_id.add(reply.tag_user);
+                }
+            }
+        }
+
+        let users = await user.findAll({
+            attributes: ["id", "username"],
+            where: {
+                id: Array.from(user_id),
+            },
+            raw: true,
+        });
+
+        let username = {};
+
+        for (const name of users) {
+            username[name.id] = name.username;
+        }
+
+        for (let i = 0; i < response.data.comment.length; i++) {
+            response.data.comment[i].username = username[response.data.comment[i].user_id];
+            let id = response.data.comment[i].tag_user;
+            if (id) {
+                response.data.comment[i].tag_username = username[id];
+            }
+            for (let j = 0; j < response.data.comment[i].reply.length; j++) {
+                response.data.comment[i].reply[j].username = username[response.data.comment[i].reply[j].user_id];
+                let id = response.data.comment[i].reply[j].tag_user;
+                if (id) {
+                    response.data.comment[i].reply[j].tag_username = username[id];
+                }
+            }
+        }
+
         return res.json(response.data);
     } catch (err) {
         return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
